02eadd828c1b839893d14080f0a8152021be6205
felkiriinya <felkiriinya@gmail.com>
2020-12-01 12:55:00 +0300

    added the routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NeighbourhoodComponent } from './components/neighbourhood/neighbourhood.component';
+const routes: Routes = [
 
-const routes: Routes = [];
+  {path: '', redirectTo: 'neighbourhood', pathMatch:'full'},
+  {path: 'neighbourhood', component: NeighbourhoodComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
