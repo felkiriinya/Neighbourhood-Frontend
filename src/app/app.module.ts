62e3a85d78211ab0d7390a4886f3b904dde73791
felkiriinya <felkiriinya@gmail.com>
2020-12-02 10:55:49 +0300

    allow the admin to view users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import {HttpClientModule} from '@angular/common/http';
-
-
+import {HttpClientModule} from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { NeighbourhoodComponent } from './components/neighbourhood/neighbourhood.component';
 import { PostFormComponent } from './components/post-form/post-form.component';
 import { UsersComponent } from './components/users/users.component';
-
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -22,7 +22,10 @@ import { UsersComponent } from './components/users/users.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    
+    NgbModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
     
   ],
   providers: [],
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -3,7 +3,7 @@
 
 export interface User {
     url: string;
-    username: string;
+    name: string;
     email: string;
     is_staff: boolean;
   }
