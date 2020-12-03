a1d0ecc47f204ef8876f4d7c1382a2e8acbb5f29
felkiriinya <felkiriinya@gmail.com>
2020-12-03 19:35:35 +0300

    added the business service and class

--- src/app/models/business.ts
+++ src/app/models/business.ts
@@ -1,2 +1,10 @@
-export class Business {
+
+export interface Business{
+        name: string,
+        description:string,
+        email:string,
+        neighbourhood:number,
+        location:string
+
+
 }
